0790fbd11742dd32f37e987a2e5acc3915a91bfb
kykapple <kykapple@naver.com>
2021-07-19 13:58:41 +0900

    ignore

--- Javascript/CRUD_DEMO/models/UserRepository.js
+++
@@ -1,9 +0,0 @@
-const db = require('./index.js');
-
-db.sequelize.sync({ force: false})
-    .then(() => {
-        console.log('연결?');
-    })
-    .catch(error => {
-        console.log("실패ㅜ");
-});
